f8a0ac2963d15e29a54261b2c5785bbec24b97c6
Alok Singh <aloksingh36757@gmail.com>
2023-12-09 09:51:14 +0530

    topcourses

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Cards from "./components/Cards";
+import Spinner from "./components/Spinner";
+import { apiUrl, filterData } from "./data";
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 
-function App() {
+
+const App = () => {
+
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+
+  //ye function lekih lenge api feacth ke liye
+  async function fetchData() {
+    setLoading(true);
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      //output----->
+      setCourses(output.data);
+    }
+    catch (error) {
+      toast.error("Network me koi problem hai");
+      //note---> ye try and catch error ka use error handling ke liye use karte hain
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen flex flex-col bg-bgDark2">
+      <div>
+        <Navbar />
+      </div>
+      <div className="bg-bgDark2">
+        <div>
+          <Filter filterData={filterData}
+            category={category}
+            setCategory={setCategory}
+          />
+        </div>
+        <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+          {
+            //this syntax means that if loading is true then spinner will execute otherwise cards will be displayed
+            loading ? (<Spinner />) : (<Cards courses={courses} category={category} />)
+          }
+        </div>
+      </div>
     </div>
+
   );
-}
+};
+
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card from './Card';
+import { useState } from 'react';
+
+
+const Cards = (props) => {
+    let courses = props.courses;
+    let category = props.category;
+    const [likedCourses, setLikedCourses] = useState([]);
+
+
+    function getCourses() {
+        if (category === "All") {
+            //all courses naam ka ek array bana liye hai jisme data stored hoga
+            let allCourses = [];
+            Object.values(courses).forEach(array => {
+                array.forEach(courseData => {
+                    allCourses.push(courseData);
+                })
+            })
+            return allCourses;
+        }
+        else {
+            //main sirf specific category ka data pass
+            return courses[category];
+        }
+    }
+
+
+
+    return (
+        <div className="flex flex-wrap justify-center gap-4 mb-4" >
+            {
+                getCourses().map((course) => (
+                    <Card key={course.id}
+                        course={course}
+                        likedCourses={likedCourses}
+                        setLikedCourses={setLikedCourses} />
+
+                ))
+            }
+        </div>
+    );
+};
+
+export default Cards;
